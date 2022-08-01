8b990bd6be62a950142ff6d0b2634a04b230834b
MuhammadRamadhan12 <102212415+MuhammadRamadhan12@users.noreply.github.com>
2022-08-01 16:28:52 +0700

    Add files via upload

---
+++ RotateScr.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScr : MonoBehaviour
+{
+    public float speed = 1f;
+    private bool putar;
+
+    private string sumbu;
+    // Update is called once per frame
+    void Update()
+    {
+        if (putar)
+        {
+            if(sumbu.ToUpper().Equals("X"))
+            {
+                transform.Rotate(Vector3.right * speed);
+            }
+            if(sumbu.ToUpper().Equals("Y"))
+                transform.Rotate(Vector3.up * speed);
+            if (sumbu.ToUpper().Equals("Z"))
+                transform.Rotate(Vector3.forward * speed);
+        }
+    }
+
+    public void RotateCube(string sumbu)
+    {
+        this.sumbu = sumbu;
+        putar = true;
+    }
+    public void StopRotation()
+    {
+        putar = false;
+    }
+}
---
+++ ShowHide.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowHide : MonoBehaviour
+{
+    public GameObject hideObject, showObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Jalankan()
+    {
+        hideObject.SetActive(false);
+        showObject.SetActive(true);
+    }
+
+}
